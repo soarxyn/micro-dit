269ffdeb5fae690839858fdd208101d40343b3e3
Gabriel Macias <macias.gabriel@proton.me>
2026-04-14 16:38:03 +0200

    Update data loading

--- src/micro_dit/data.py
+++ src/micro_dit/data.py
@@ -1,29 +1,32 @@
 import lightning as L
-import numpy as np
 import torch
 from torch.utils.data import DataLoader, Dataset
 from torchvision.transforms import v2
+from safetensors.torch import safe_open
+import multiprocessing
+
+multiprocessing.set_start_method("fork")
 
 _latent_transforms = v2.Compose([v2.RandomHorizontalFlip()])
 
 
 class LatentDataset(Dataset):
-    def __init__(self, npy_file: str):
-        # TODO: Solve Pickle reading problem in NPY
-        self.data = np.load(npy_file, mmap_mode="r", allow_pickle=True)
+    def __init__(self, path: str):
+        self.data = safe_open(path, framework="pt", device="cpu")
+        self.num_samples = self.data.get_tensor("indices").shape[0]
 
     def __getitem__(self, index) -> torch.Tensor:
-        item = torch.from_numpy(self.data[index].copy())
-        return _latent_transforms(item)
+        indices = self.data.get_slice("indices")[index]
+        return _latent_transforms(indices)
 
     def __len__(self):
-        return len(self.data)
+        return self.num_samples
 
 
 class LatentDataModule(L.LightningDataModule):
     def __init__(
         self,
-        npy_file: str,
+        path: str,
         batch_size: int = 128,
         num_workers: int = 4,
         pin_memory: bool = True,
@@ -31,7 +34,7 @@ class LatentDataModule(L.LightningDataModule):
     ):
         super().__init__()
 
-        self.npy_file = npy_file
+        self.path = path
 
         self.batch_size = batch_size
         self.num_workers = num_workers
@@ -39,7 +42,7 @@ class LatentDataModule(L.LightningDataModule):
         self.drop_last = drop_last
 
     def setup(self, stage: str = ""):
-        self.train_dataset = LatentDataset(self.npy_file)
+        self.train_dataset = LatentDataset(self.path)
 
     def train_dataloader(self):
         return DataLoader(
